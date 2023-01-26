80d0f3f2ee47a4aa8ee79adfceca83815ff5d567
Darkspiet <dreessen.kevin@gmail.com>
2023-01-26 16:24:04 +0100

    Update navbar.php

--- php/Dreessen/navbar.php
+++ php/Dreessen/navbar.php
@@ -18,7 +18,7 @@ if(isset($_SESSION['roles_id'])){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></scipt>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link  rel="stylesheet" href="../css/style.css">
-
+    
     <!-- Styles von Kevin -->
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/blog/">
     <!-- Bootstrap core CSS -->
