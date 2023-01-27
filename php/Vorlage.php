078b4bb710fba7891de30f6392b27e5bea96b4d6
turk-Dangolover <115842768+turk-Dangolover@users.noreply.github.com>
2023-01-27 12:26:14 +0100

    Update Vorlage.php

--- php/Vorlage.php
+++ php/Vorlage.php
@@ -1,3 +1,6 @@
+Erstellt von Cem Cetin
+
+/////////////////////////////////////
 
 include navbar
 <div class="container">
@@ -11,3 +14,29 @@ include navbar
     </div>
 </div>
 include footer
+
+/////////////////////////////////////
+Wie verwendet man Session_start()?
+
+Wenn ihr die navbar integriert habt, müsst ihr kein session_start() befehl mehr ausführen.
+Folgende Variabeln stehen zur Auswahl, diese werden gesetzt, wenn ein User sich einlogt.
+
+$_SESSION['email'] = $email;
+$_SESSION['passwort'] = $password;
+$_SESSION['user_id'] = $row['user_id'];
+$_SESSION['roles_id'] = $row['roles_id'];
+$_SESSION['login'] = TRUE;
+
+Ihr könnt sie entweder direkt verwenden, oder besser, ihr speichert Sie in eine Variabel.
+Wenn der User nicht eingelogt ist, könnt ihr das mit einem isset() Befehl überprüfen.
+Andere möglichkeit gibt es auch, aber das ist die einfachste.
+
+/////////////////////////////////////
+
+Bitte schließt alle Sicherheitslücken mithilfe der Session_start() Funktion.
+Kein User darf auf eine Seite zugreifen, auf die er keine Rechte hat.
+
+Da die header Funktion nicht immer geht, empfehle ich euch, in eine if-Abfrage einfach include_once "../Cetin/401.php"; einzufügen.
+Vergisst nicht die If-Abfrage mit einem return oder ähnlichen Befehlen zu schließen!!!!!
+
+/////////////////////////////////////
