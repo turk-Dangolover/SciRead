07f25adbf6618018753738398c659fe2dfc94184
turk-Dangolover <115842768+turk-Dangolover@users.noreply.github.com>
2023-01-27 12:45:57 +0100

    Update page_admin.php

--- php/Cetin/page_admin.php
+++ php/Cetin/page_admin.php
@@ -10,9 +10,8 @@ Beschreibung: Bereich für den Admin mit allen Funktionen
     <meta http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <link  rel="stylesheet" href="../css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
         <title>SciReader | Admin Bereich</title>
 </head>
 <body>
@@ -21,34 +20,30 @@ Beschreibung: Bereich für den Admin mit allen Funktionen
 include_once "../Dreessen/navbar.php";
 // Verbindung zur Datenbank
 require 'connect.php';
-// Holt sich die Rolle des Users
+// Prüft ob der User eingelogt ist
 if(!(isset($_SESSION['roles_id']))){
     include_once "401.php";
     return;
 }
+// Holt die Rolle des Users aus der Session
 $role = $_SESSION['roles_id'];
-// Falls der User ein Admin ist, wird ihn der Inhalt der Seite angezeigt
+// Falls der User kein Admin ist, wird Ihn eine Fehlermeldung angezeigt
 if(!($role == '1')){
-    // Falls der User kein Admin ist, wird Ihn eine Fehlermeldung angezeigt
     include_once "401.php";
     return;
     }
  ?>
-<!-- Inhalt der Website -->
+<!-- Falls der User ein Admin ist, wird ihn der Inhalt der Seite angezeigt -->
 <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3>Admin Bereich</h3>
-                </div>
-                <div class="card-body text-center container-fluid">
-                    <div class="list-group">
-                        <h4 class="list-group-item list-group-item-action active">Listen</h4>
-                        <a href="page_admin_accounts.php" class="list-group-item list-group-item-action ">Liste aller Accounts</a>
-                        <a href="../Lau/Administrationsbereich.php" class="list-group-item list-group-item-action ">Datenbank Entitäten</a>
-                    </div>
-                </div>
+    <div class="card">
+        <div class="card-header">
+            <h3>Admin Bereich</h3>
+        </div>
+        <div class="card-body text-center container-fluid">
+            <div class="list-group">
+                <h4 class="list-group-item list-group-item-action active">Listen</h4>
+                <a href="page_admin_accounts.php" class="list-group-item list-group-item-action ">Liste aller Accounts</a>
+                <a href="../Lau/Administrationsbereich.php" class="list-group-item list-group-item-action ">Datenbank Entitäten</a>
             </div>
         </div>
     </div>
