523235ec8b867f255646a671d61b2c060964b360
turk-Dangolover <115842768+turk-Dangolover@users.noreply.github.com>
2023-01-27 12:54:12 +0100

    Update change_password

--- php/Cetin/page_admin.php
+++ php/Cetin/page_admin.php
@@ -8,10 +8,10 @@ Beschreibung: Bereich für den Admin mit allen Funktionen
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-        <title>SciReader | Admin Bereich</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>SciReader | Admin Bereich</title>
 </head>
 <body>
 <?php
