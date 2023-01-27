622866ba33412a1086b11f2ff97f30730df00aa1
turk-Dangolover <115842768+turk-Dangolover@users.noreply.github.com>
2023-01-27 12:39:47 +0100

    Update page_admin_accounts.php

--- php/Cetin/page_admin_accounts.php
+++ php/Cetin/page_admin_accounts.php
@@ -1,6 +1,5 @@
 <!--
 Erstellt von Cem Cetin
-Datum: 07.01.2023
 Beschreibung: Website für die Verwaltung der Benutzer
 -->
 <!DOCTYPE html>
@@ -10,9 +9,8 @@ Beschreibung: Website für die Verwaltung der Benutzer
     <meta http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link  rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <title> SciReader | Admin AccountView</title>
 </head>
 <body>
@@ -22,65 +20,62 @@ Beschreibung: Website für die Verwaltung der Benutzer
 include_once "../Dreessen/navbar.php";
 // Verbindung zur Datenbank
 require 'connect.php';
-// Holt die Rolle des Users aus der Session
+// Prüft ob der User eingelogt ist
 if(!(isset($_SESSION['roles_id']))){
     include_once "401.php";
     return;
 }
+// Holt die Rolle des Users aus der Session
 $role = $_SESSION['roles_id'];
 // Falls der User ein Admin ist, wird ihn der Inhalt der Seite angezeigt
 if($role == '1'){
     // Holt alle Accounts aus der Datenbank
    $query = 'SELECT * FROM users';
    // Führt die Abfrage aus
-   $res = $dbh->prepare($query);
-   $res->execute();
+   $stmt = $dbh->prepare($query);
+   $stmt->execute();
    ?>
     <!-- Hier wird der Inhalt der Seite angezeigt -->
 <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3>Admin Accounts</h3>
-                    </div>
-                    <div class="card-body text-center container-fluid">
-                        <table class="table table-striped ">
-                        <thead>
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Role</th>
-                                <th scope="col">Bearbeiten</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php 
-                            //Hier wird die Tabelle mit den Accounts ausgegeben
-                            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-                                echo '<tr>';
-                                echo '<td>' . $row['user_id'] . '</td>';
-                                echo '<td>' . $row['email'] . '</td>';
-                                //Da es nur zwei Rollen gibt, wurde hier eine if-else-Abfrage verwendet und keine foreach-Schleife
-                                if ($row['roles_id'] == 1) {
-                                    echo '<td>Admin</td>';
-                                } else if ($row['roles_id'] == 2){
-                                    echo '<td>User</td>';
-                                } else {
-                                    echo '<td>Kein Rolle vergeben</td>';
-                                }
-                                echo '<td><button class="btn btn-danger" onclick="deleteAccount(' . $row['user_id'] . ')">Löschen</button></td>';
-
-                                echo '</tr>';                                                        
-                            }
-                            ?>
-                        </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+    <div class="card">
+        <div class="card-header">
+            <h3>Admin Accounts</h3>
         </div>
+        <div class="card-body text-center container-fluid">
+            <table class="table table-striped ">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Bearbeiten</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php 
+                    //Hier wird die Tabelle mit den Accounts ausgegeben
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        echo '<tr>';
+                        echo '<td>' . $row['user_id'] . '</td>';
+                        echo '<td>' . $row['email'] . '</td>';
+                        //Da es nur zwei Rollen gibt, wurde hier eine if-else-Abfrage verwendet und keine foreach-Schleife
+                        if ($row['roles_id'] == 1) {
+                        echo '<td>Admin</td>';
+                        } else if ($row['roles_id'] == 2){
+                        echo '<td>User</td>';
+                        } else {
+                        echo '<td>Kein Rolle vergeben</td>';
+                        }
+                        echo '<td><button class="btn btn-danger" onclick="deleteAccount(' . $row['user_id'] . ')">Löschen</button></td>';
+                        echo '</tr>';                                                        
+                        }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
+<!-- Footer wird eingebunden -->
 <?php
 include_once "../Dreessen/footer.php";
 ?>
@@ -91,6 +86,7 @@ include_once "../Dreessen/footer.php";
    }
 ?>
 </body>
+<!--Pop Up der zum Löschskript weiterleitet -->
 <script type="text/javascript">
 function deleteAccount(id) {
     var id = id;
