d54ee135e2049ee613388711fee4f4da6aee7a3e
Kimuna <lukekliefoth@gmail.com>
2023-01-28 14:50:45 +0100

    Fixed?

--- php/Lau/addFachbereich display.php
+++ php/Lau/addFachbereich display.php
@@ -1,60 +1,62 @@
 <form method="post">
 
-<div class="col-auto">
-      <label for="search-filter" class="col-form-label">Sortieren:</label>
-   <select  class="custom-select mr-sm-2" id="sort" name="sort">
+  <div class="col-auto">
+    <label for="search-filter" class="col-form-label">Sortieren:</label>
+    <select class="custom-select mr-sm-2" id="sort" name="sort">
       <option value="fachbereich_id">ID</option>
       <option value="fachbereich">Fachbereich</option>
-      
-   </select>
-  <div class="col-auto"> <div class="col-auto">
-  <button type="submit" class="btn btn-light">Abschicken</button>
-  </div>
-</div>
-                
+
+    </select>
+    <div class="col-auto">
+      <div class="col-auto">
+        <button type="submit" class="btn btn-light">Abschicken</button>
+      </div>
+    </div>
+
 </form>
 
-<div class="table-responsive"><table class="table">
-<thead>
-<tr>
-  <th>ID</th>
-  <th>Fachbereich</th>
-  <th>Kommentar</th>
-  <th></th>
-</tr>
-</thead>
-<br>
-<tbody>
-<?php
-require_once 'connect-server.php';
-if(isset($_POST['sort'])){
-  $varsort = $_POST['sort'];
-  $sql= "SELECT * FROM fachbereich ORDER BY $varsort";
-  $typen= executeSQL($sql)->fetchAll();
-  }else{
-  $sql = "SELECT * FROM fachbereich ORDER BY fachbereich_id";
-  $typen= executeSQL($sql)->fetchAll();
-  }
-foreach ($typen as $row) {
-  echo "<tr>";
-  echo "<td>".$row['fachbereich_id']."</td>";
-  echo "<td>".$row['fachbereich']."</td>";
-  echo "<td>".$row['comment']."</td>";
-  echo '<td><form action="fachbereich-edit-form.php" method="GET">';
-  echo '<input type="hidden" name="fachbereich_id" value="'.$row['fachbereich_id'].'">';
-  echo '<input type="hidden" name="fachbereich" value="'.$row['fachbereich'].'">';
-  echo '<input type="hidden" name="comment" value="'.$row['comment'].'">';
-  echo '<input type="submit" name="edit" value="edit">';
-  echo '</form></td>';
-  echo '<td><form action="loeschenrow-fachbereich.php" method="post">';
-  echo '<input type="hidden" name="fachbereich_id" value="'.$row['fachbereich_id'].'">';
-  echo '<input type="submit" class="btn btn-danger" value="Löschen" onclick="return confirm(\'Sicher das Sie diesen Eintrag löschen möchten?\')">';
-  echo '</form></td>';
-  echo "</tr>";
-}
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Fachbereich</th>
+        <th>Kommentar</th>
+        <th></th>
+      </tr>
+    </thead>
+    <br>
+    <tbody>
+      <?php
+      require_once 'connect-server.php';
+      if (isset($_POST['sort'])) {
+        $varsort = $_POST['sort'];
+        $sql = "SELECT * FROM fachbereich ORDER BY $varsort";
+        $typen = executeSQL($sql)->fetchAll();
+      } else {
+        $sql = "SELECT * FROM fachbereich ORDER BY fachbereich_id";
+        $typen = executeSQL($sql)->fetchAll();
+      }
+      foreach ($typen as $row) {
+        echo "<tr>";
+        echo "<td>" . $row['fachbereich_id'] . "</td>";
+        echo "<td>" . $row['fachbereich'] . "</td>";
+        echo "<td>" . $row['comment'] . "</td>";
+        echo '<td><div class="row"><form action="fachbereich-edit-form.php" method="GET" class="px-2">';
+        echo '<input type="hidden" name="fachbereich_id" value="' . $row['fachbereich_id'] . '">';
+        echo '<input type="hidden" name="fachbereich" value="' . $row['fachbereich'] . '">';
+        echo '<input type="hidden" name="comment" value="' . $row['comment'] . '">';
+        echo '<input type="submit" class="btn btn-danger" name="edit" value="Edit">';
+        echo '</form>';
+        echo '<form action="loeschenrow-fachbereich.php" method="post">';
+        echo '<input type="hidden" name="fachbereich_id" value="' . $row['fachbereich_id'] . '">';
+        echo '<input type="submit" class="btn btn-danger" value="Löschen" onclick="return confirm(\'Sicher das Sie diesen Eintrag löschen möchten?\')">';
+        echo '</form></div></td>';
+        echo "</tr>";
+      }
 
-?>
+      ?>
 
-</tbody>
-</table>
+    </tbody>
+  </table>
 </div>
--- php/Lau/addFachbereich.php
+++ php/Lau/addFachbereich.php
@@ -1,54 +1,59 @@
 <!Doctype html>
 <html lang="de">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-    <body>
-    <?php include_once "../Dreessen/navbar.php"; ?>
-    <div class="container">
-                        <div class="card">
-                            <div class="card-header">
-                                <h3>Fachbereich</h3>
-                         </div>
-                            
-
-<nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
-   <li class="breadcrumb-item"><a href="Administrationsbereich.php">Adminbereich</a></li>  
-   <li class="breadcrumb-item active" aria-current="page">Fachbereich</li>
-    <li class="breadcrumb-item"><a href="addType.php">Typ</a></li>
-    <li class="breadcrumb-item"><a href="addVerlag.php">Verlag</a></li>
-  </ol>
-</nav>
-<forms tyle="margin-left:20px" action="savefachbereich.php" method="post">
-<div class="row">
-      <div class="col-md-4 mb-3">
-      <label for="author">Fachbereich:</label>
-      <input type="text" class="form-control" name="fachbereich"  required>                            
+<body>
+  <?php include_once "../Dreessen/navbar.php"; ?>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h3>Fachbereich</h3>
       </div>
-      <div class="col-md-4 mb-3">
-      <label for="Kommentar">Anmerkungen</label>
-      <input type="text"  class="form-control" name="comment" id="comment">
+
+      <div class="card-body">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="Administrationsbereich.php">Adminbereich</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Fachbereich</li>
+            <li class="breadcrumb-item"><a href="addType.php">Typ</a></li>
+            <li class="breadcrumb-item"><a href="addVerlag.php">Verlag</a></li>
+          </ol>
+        </nav>
+        <form tyle="margin-left:20px" action="savefachbereich.php" method="post">
+          <div class="row align-items-end">
+            <div class="col-md-4 mb-3">
+              <label for="author">Fachbereich:</label>
+              <input type="text" class="form-control" name="fachbereich" required>
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="Kommentar">Anmerkungen</label>
+              <input type="text" class="form-control" name="comment" id="comment">
+            </div>
+            <div class="col-md-4 mb-3">
+              <input type="submit" value="Upload" name="submit" class="btn btn-light" onclick="alert('Neuer Fachbereich hinzugefügt!'); location.href = 'addFachbereich.php'; return true;" /><br><br>
+            </div>
+          </div>
+
+        </form>
+
+
+
+        <br><br>
+        <?php
+        include('addFachbereich display.php');
+        ?>
       </div>
-</div>
-<input type="submit" value="Upload" name="submit"  class="btn btn-light" 
-  onclick="alert('Neuer Fachbereich hinzugefügt!'); location.href = 'addFachbereich.php'; return true;" /><br><br>
-
-</form>
+    </div>
+  </div>
 
 
-
-<br><br>
-<?php
-include('addFachbereich display.php');
-?>
-    
-
-</body>
-<form>
   <?php include_once '../Dreessen/footer.php' ?>
-</form>
+</body>
+
+
 </html>
