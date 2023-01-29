de1f311fee5924ae6ef0a03de99fe50300f1d9ed
Darkspiet <dreessen.kevin@gmail.com>
2023-01-29 17:48:03 +0100

    footer into a container

--- php/Dreessen/footer.php
+++ php/Dreessen/footer.php
@@ -1,11 +1,13 @@
-<footer class="blog-footer sticky-bottom w-100">
-    <p>This Website was Build by</p>
+<div class="container-xxl">
+    <footer class="blog-footer">
+        <p>This Website was Build by</p>
 
-    <a href="#" class="d-inline-block">Cem</a>,
-    <a href="#" class="d-inline-block">Viktoria</a>,
-    <a href="#" class="d-inline-block">Jonas</a>,
-    <a href="#" class="d-inline-block">Kevin</a>
-    <!-- <p>
-        <a href="#">Back to top</a>
-        </p> -->
-</footer>
+        <a href="#" class="d-inline-block">Cem</a>,
+        <a href="#" class="d-inline-block">Viktoria</a>,
+        <a href="#" class="d-inline-block">Jonas</a>,
+        <a href="#" class="d-inline-block">Kevin</a>
+        <!-- <p>
+            <a href="#">Back to top</a>
+            </p> -->
+    </footer>
+</div>    
