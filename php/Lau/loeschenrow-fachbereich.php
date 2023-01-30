0beff35d250c9d3c0bb5fc703b25c45d70230f1a
vaaaaah <viktorialau2003@gmail.com>
2023-01-30 22:45:32 +0100

    viktoria

--- php/Lau/loeschenrow-fachbereich.php
+++ php/Lau/loeschenrow-fachbereich.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'connect-server.php';
 $id = $_POST['fachbereich_id'];
-$sql = "UPDATE literatur SET publisher_id = :filler WHERE publisher_id = :id";
+$sql = "UPDATE literatur SET fachbereich_id = :filler WHERE fachbereich_id = :id";
 $stmt = $dbConnection->prepare($sql);
 $stmt->bindValue(':filler', NULL);
 $stmt->bindValue(':id', $id);
