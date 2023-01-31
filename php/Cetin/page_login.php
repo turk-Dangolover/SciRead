8484612c72b2b3ab27515d2412433f8dba8aab99
turk-Dangolover <115842768+turk-Dangolover@users.noreply.github.com>
2023-01-31 12:02:11 +0100

    Update page_login.php

--- php/Cetin/page_login.php
+++ php/Cetin/page_login.php
@@ -22,9 +22,11 @@ Beschreibung: Login Seite
 ?>
 <div class="container py-5">
   <div class="d-flex justify-content-center">
+        <!-- col-xl-10 beschränkt die Größe bei großen Bildschirmen -->
     <div class="col col-xl-10">
       <div class="card" style="border-radius: 1rem;">
         <div class="row">
+          <!-- Bei kleinen Bildschirmen wird das Bild ausgeblendet -->
           <div class=" col-lg-5 d-none d-md-block">
             <img src="https://images.pexels.com/photos/1907784/pexels-photo-1907784.jpeg?auto=compress&cs=tinysrgb&w=1600"
             alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
@@ -39,11 +41,11 @@ Beschreibung: Login Seite
               </div>
               <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Melde dich mit in dein Account ein </h5>
               <form method="post" action="function_login.php">
-                <div class="form-outline mb-4">
+                <div class="mb-4">
                   <input type="email" id="email" name="email" class="form-control form-control-lg" />
                   <label class="form-label" for="email">Email Adresse</label>
                 </div>
-                <div class="form-outline mb-4">
+                <div class="mb-4">
                   <input type="password" id="kennwort" name="kennwort" class="form-control form-control-lg" />
                   <label class="form-label" for="kennwort">Kennwort</label>
                 </div>
