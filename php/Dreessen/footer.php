0ab6a6a3506c08d871d81eb0657b211755857adf
Darkspiet <dreessen.kevin@gmail.com>
2023-01-31 15:27:31 +0100

    Update footer.php

--- php/Dreessen/footer.php
+++ php/Dreessen/footer.php
@@ -1,5 +1,5 @@
 
-<footer class="footer mt-auto py-3 bg-dark text-center text-white ">
+<footer class="footer mt-auto py-3 text-center" style="background-color: #f1f1f1;">
     <div class="Container py-3">
         <p>This Website was Build by</p>
 
@@ -8,5 +8,8 @@
         <a href="#" class="d-inline-block">Jonas</a>,
         <a href="#" class="d-inline-block">Kevin</a>
     <div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <a class="text-dark">SciRead</a>
+  </div>    
 </footer>
   
